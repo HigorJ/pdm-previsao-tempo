5fb6ed7bcac9d126153d92adfe299a103d56fcc2
higorj <higor_sgomes@outlook.com>
2022-05-06 19:46:56 -0300

    Adição do .env

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { 
+  API_LANGUAGE
+} from '@env';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{API_LANGUAGE}</Text>
     </View>
   );
 }
