817000138d008265a520434b0e6060bece993ffc
higorj <higor_sgomes@outlook.com>
2022-05-06 20:12:36 -0300

    Interface grafica e inputs

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { 
   API_LANGUAGE
 } from '@env';
 
+import { 
+  Button,
+  FlatList, 
+  StyleSheet, 
+  Text,
+  TextInput,
+  View 
+} from 'react-native';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{API_LANGUAGE}</Text>
+      <View style={styles.cidadeView}>
+        <TextInput
+          style={styles.cidadeTextInput}
+          placeholder="Digite o nome da cidade"
+        />        
+        <Button 
+          title="OK"
+        />
+      </View>
+
+      <FlatList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
   },
+  cidadeView: {
+    padding: 12,
+    marginBottom: 8
+  },
+  cidadeTextInput: {
+    padding: 12,
+    borderBottomColor: '#FF9800',
+    borderBottomWidth: 2,
+    marginBottom: 4,
+    textAlign: 'center',
+  }
 });
